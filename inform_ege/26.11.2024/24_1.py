6a12313b24b067d9bb6f4f14800004b098ddb212
GRitger <131749350+GRitger@users.noreply.github.com>
2024-11-26 11:29:30 +0300

    Update 24_1.py

--- inform_ege/26.11.2024/24_1.py
+++ inform_ege/26.11.2024/24_1.py
@@ -6,7 +6,16 @@ s = s.replace("**", "* *")
 s = s.replace("*0", "* 0")
 
 a = s.split()
+arr = []
 
 for i in a:
     while len(i) and i[0] in "0*":
-        i = 
+        i = i[1:]
+    while len(i) and i[-1] in "*":
+        i = i[:-1]
+    arr.append(i)
+
+arr.sort(key = len)
+
+for i in arr:
+    print(i, len(i)-i.count("*"))
