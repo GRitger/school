57cf0c8694797f52a22596d8ea1f8ac3023dffde
GRitger <nik@eman.su>
2025-02-11 21:26:20 +0300

    12.02.2025

---
+++ inforamatic/12.02.2025/Home/2.py
@@ -0,0 +1,24 @@
+from functools import lru_cache
+
+
+def g(x):
+    return x + 3, x * 2
+
+
+@lru_cache(None)
+def f(x, y):
+    if x + y >= 157:
+        return "+"
+    if any(f(i, y) == "+" for i in g(x)) or any(f(x, i) == "+" for i in g(y)):
+        return "p1"
+    if all(f(i, y) == "p1" for i in g(x)) and all(f(x, i) == "p1" for i in g(y)):
+        return "v1"
+    if any(f(i, y) == "v1" for i in g(x)) or any(f(x, i) == "v1" for i in g(y)):
+        return "p2"
+    if all(f(i, y) in ["p1", "p2"]  for i in g(x)) and all(f(x, i) in ["p1", "p2"]  for i in g(y)):
+        return "v2"
+
+
+for i in range(1, 113):
+    if f(i, 43) == "v2":
+        print(i, f(i, 43))
