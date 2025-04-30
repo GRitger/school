291ffb3b7195183284ba0de2b8e9b15b722375ba
GRitger <nik@eman.su>
2025-04-30 10:29:52 +0300

    320

--- test.py
+++ test.py
@@ -1,23 +1,23 @@
 from functools import lru_cache
 import sys
 
-sys.setrecursionlimit(100000000)
-print(sys.getrecursionlimit())
-
 @lru_cache(None)
 def r(x):
     s = 0
     while x:
-        s += x% 10
+        s += x % 10
         x = x//10
     return s
 
 @lru_cache(None)
-def f(a,b, n):
-    if a <= 11 or n > 999:
+def f(a,b, n, h=0):
+    if a < 11 or n > 1:
         return 0
     if a == b:
+        print(h)
         return 1
-    return f(a+r(a)*3,b,n+1)+f(a-r(a)*3,b,n+1)
 
-print(f(65,11,0))
+
+    return f(a+r(a)*3,b,n+1, a)+f(a-r(a)*3,b,n+1, a)
+
+print(f(65,32,0))
