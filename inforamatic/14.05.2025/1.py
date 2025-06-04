b83ae0ba740290cfd83d6d6219f5cc20c7d9436e
GRitger <nik@eman.su>
2025-06-04 10:06:40 +0300

    04.06.2025

--- inforamatic/14.05.2025/1.py
+++ inforamatic/14.05.2025/1.py
@@ -2,4 +2,4 @@ with open("1.txt") as f:
     st = f.readline()
 c = 0
 i = 0
-j =
+j = 0
--- qwerty.py
+++ qwerty.py
@@ -1,4 +1,11 @@
-import requests
-for i in range(40):
-    r = requests.get('http://31.135.225.58:10040')
-    print(i, r)
+a = 4
+b = 5
+
+for i in range(1, 400):
+    if i % 2:
+        a -=1
+        b += 2
+    else:
+        a += 2
+        b -= 1
+    print(i, a,b)
